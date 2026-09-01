da75a354225e01a4e68c65cca5dcc67990c1c6e8
rcastroguerra <raydel.castroguerra@gmail.com>
2026-09-01 11:49:34 -0400

    Agregando cambios y yml

---
+++ upload github.R
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=76CtlssWnTA
+# https://gist.github.com/irwingss/c7993e545654740ef21df3ccba9b2add
+
+#solo se usa una vez
+#install.packages ("usethis")
+#library(usethis)
+#use_git()
+#use_github()
+use_github_pages()
+
+
+#
